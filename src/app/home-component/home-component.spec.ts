77fb26371e0254e5d9b91f8cafa39c8477e1546f
Piotr Bączek <pgolasz@gmail.com>
2026-07-10 20:02:45 +0200

    pass tests

--- src/app/home-component/home-component.spec.ts
+++ src/app/home-component/home-component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home-component';
+import { RouterModule } from '@angular/router';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,12 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent],
+      declarations: [
+        HomeComponent
+      ],
+      imports: [
+        RouterModule.forRoot([]),
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
